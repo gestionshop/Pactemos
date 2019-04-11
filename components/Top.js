0a2307752625063866aef8c2bb3e838dd5c2960c
Edwin Gonzales <edwinpgm@gmail.com>
2019-04-10 22:51:33 -0500

    fix errors

--- components/Top.js
+++ components/Top.js
@@ -36,10 +36,11 @@ class Top extends Component {
   submit = (e) => {
     e.preventDefault()
 
-    // const url = 'http://localhost:3001/api/pactemos/cotizacion'
-    const url = 'https://gestionshop.co/api/pactemos/cotizacion'
+    const url = 'http://localhost:3001/api/pactemos/domicilio'
+    // const url = 'https://gestionshop.co/api/pactemos/cotizacion'
     axios.post(url, this.state).then(res => {
       this.setState({ submited: true })
+      console.log('ENVIADOO')
       window.location.replace('/gracias')
     }).catch(error => {
       alert('Ocurrio un error y no se pudo enviar la información')
