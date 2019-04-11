0fe428d4e6a97fa0d7218c0dd98423a7697ab926
Arnold Torres <sr.arnoldt@gmail.com>
2019-04-10 21:31:18 -0500

    foto24

--- pages/index.js
+++ pages/index.js
@@ -46,8 +46,8 @@ class Home extends Component {
   submit = (e) => {
     e.preventDefault()
 
-    // const url = 'http://localhost:3001/api/pactemos/cotizacion'
-    const url = 'https://gestionshop.co/api/pactemos/cotizacion'
+    const url = 'http://localhost:3001/api/pactemos/cotizacion'
+    // const url = 'https://gestionshop.co/api/pactemos/cotizacion'
     axios.post(url, this.state).then(res => {
       this.setState({ submited: true })
     }).catch(error => {
